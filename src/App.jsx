5de6f1ab1d7b5b79cbdfd260b749e2b975bbe730
AniketDandhare22 <aniketdandhare22@gmail.com>
2025-12-18 21:12:31 +0530

    Main container Created

---
+++ src/App.jsx
@@ -0,0 +1,118 @@
+import { useEffect, useState } from "react";
+import Card from "./component/card.jsx";
+import axios from 'axios'
+import backG from "../backG.png";
+
+function App() {
+  const [userData,setUserData] =useState([]);
+  const [index,setIndex] =useState(1);
+  const [loading, setLoading] = useState(false);
+  const getData = async () => {
+    try {
+      setLoading(true);
+      const response = await axios.get(
+        `https://picsum.photos/v2/list?page=${index+1}&limit=20`
+      );
+      setUserData(response.data);
+    } catch (err) {
+      console.error(err);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(function(){
+    getData()
+  },[index])
+
+  return (
+    <div className="h-screen w-full  text-white">
+      
+      {/* Page Wrapper */}
+      <div className="flex h-full flex-col bg-['./backG.png']">
+      <img
+        src={backG}
+        alt="background"
+        className="absolute inset-0 w-full h-full object-cover -z-10"
+      />
+
+        {/* Navbar */}
+        <div className="glass-effect h-16 rounded-b-lg flex items-center justify-center text-2xl shrink-0">
+          Navbar
+        </div>
+
+        {/* Content */}
+        <div className="flex flex-1 overflow-hidden p-1 gap-2">
+
+          {/* Sidebar */}
+          <div className="glass-effect w-[20%] lg:w-[15%] p-10 rounded-lg shrink-0 ">
+            <h2 className="text-3xl font-bold">Sidebar</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor quisquam laborum vel vero est odit alias, temporibus reprehenderit debitis sunt dignissimos perferendis nemo adipisci voluptate et incidunt ullam, fuga hic, deleniti nam! Deserunt commodi aut tempore labore, perspiciatis eos? Harum reiciendis nesciunt laboriosam praesentium recusandae. Odit ad consequatur debitis fugiat quos incidunt aspernatur tempora, consequuntur beatae, et iure necessitatibus modi accusantium ducimus fugit at commodi ipsum facilis. Suscipit perferendis repudiandae consectetur, quam ut eveniet totam est, commodi pariatur porro, excepturi culpa autem? Sunt corrupti, sequi accusamus quam odit ducimus voluptates voluptatibus iusto quae reprehenderit tenetur numquam accusantium</p>
+          </div>
+
+
+          {/* Main */}
+          <div className="flex flex-1 flex-col min-h-0">
+            {/* Cards */}
+            <div className="flex-1 overflow-auto m-2 pr-2 custom-scroll mb-1">
+              {/* Cards grid ONLY */}
+              <div className="grid grid-cols-[repeat(auto-fit,minmax(300px,1fr))] gap-2">
+                {loading ? (
+                  <div className="col-span-full flex justify-center items-center text-xl h-200">
+                        <div role="status">
+                            <span className="text-xs font-bold">Loading...</span>
+                            <svg
+                              className="w-10 h-10 animate-spin"
+                              viewBox="0 0 100 100"
+                              xmlns="http://www.w3.org/2000/svg"
+                            >
+                              <circle
+                                cx="50"
+                                cy="50"
+                                r="45"
+                                fill="none"
+                                stroke="rgba(255,255,255,0.25)"   // glass color
+                                strokeWidth="6"
+                                strokeLinecap="round"
+                                strokeDasharray="210"
+                                strokeDashoffset="60"
+                              />
+                            </svg>
+                        </div>
+                  </div>
+                ) : (
+                  userData.map((elem) => <Card key={elem.id} elem={elem} />)
+                )}
+              </div>
+
+              {/* Pagination BELOW grid */}
+              <div className="flex justify-center p-4 rounded-xl">
+                <button  className={`glass-effect px-8 h-10 rounded-lg mx-5 text-lg ${index === 1 ? "opacity-50 pointer-events-none" : "hover:scale-103 active:scale-97"}`}
+                  onClick={()=>{
+                    if(index>1)setIndex(index-1);
+                    setUserData([])
+                  }}
+                >Prev</button>
+                <p className="text-xl  font-bold">Page-{index}</p>
+                <button className="glass-effect px-8 p-1 h-10 rounded-lg mx-5 text-lg  hover:scale-103 active:scale-97 hover:border-none"
+                  onClick={()=>{
+                      setIndex(index+1);
+                      setUserData([])
+                    }}
+                >Next</button>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        {/* Footer */}
+        <div className="glass-effect h-12 flex items-center justify-center text-2xl font-bold shrink-0">
+          Footer
+        </div>
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/card.jsx
@@ -0,0 +1,57 @@
+import { useState } from "react";
+
+function Card({ elem }){
+  const [liked, setLiked] = useState(false);
+
+  return (
+    <>
+        <div className="group glass-effect w-full h-100 overflow-hidden rounded-2xl p-2 hover:p-1 hover:scale-101 transition-transform duration-300 ease-in-out m-1 ">
+            <div className={`cover w-full h-full opacity-83 hover:opacity-100  rounded-2xl  justify-center transition-transform duration-300 ease-in-out  flex flex-col bg-cover`} style={{
+          backgroundImage: `url(${elem.download_url})`,
+        }}>
+                <div className=" h-[75%]"></div>
+                <div className="mx-5 text-gray-300 mb-2 text-xs group-hover:font-bold  ">{elem.author}</div>
+                <div className="flex flex-row w-full justify-between px-4">
+                  <button className="glass-effect rounded-xl  text-white font-light px-9 py-1 active:scale-95 active:font-bold text-lg hover:scale-105 
+                      transition-transform duration-200 ">Download</button>
+                  <button
+                    onClick={() => setLiked(!liked)}
+                    className="
+                      glass-effect text-white 
+                      w-9 h-9 rounded-3xl 
+                      flex justify-center items-center
+                      transition-transform duration-200
+                      hover:scale-110 active:scale-90
+                    "
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="20"
+                      height="20"
+                      viewBox="0 0 16 16"
+                      className={`
+                        transition-colors duration-300
+                        ${liked ? "fill-red-400" : "fill-none stroke-white"}
+                      `}
+                    >
+                      <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01z" />
+                    </svg>
+                  </button>
+                </div>
+            </div>
+
+        </div>
+    </>
+  );
+}
+
+export default Card;
+
+
+          // absolute inset-0
+          // bg-gradient-to-r
+          // from-[rgba(255,99,8,0.35)]
+          // via-[rgba(189,201,230,0.35)]
+          // to-[rgba(151,196,255,0.35)]
+          // [mask-image:radial-gradient(ellipse_at_top,black_40%,transparent_75%)]
+          // [-webkit-mask-image:radial-gradient(ellipse_at_top,black_40%,transparent_75%)]
